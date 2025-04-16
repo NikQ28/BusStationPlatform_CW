b8bd2dfcd966c17a682aaaccffbc0fe8e8c806ae
Nikolay <stebnev.nikoly280205@yandex.ru>
2025-04-16 03:04:14 +0300

    add xml-documentation

--- Domains/DTO/RouteDTO.cs
+++ Domains/DTO/RouteDTO.cs
@@ -1,9 +1,25 @@
-﻿namespace BusStationPlatform.Domains.DTO
+﻿using System;
+
+namespace BusStationPlatform.Domains.DTO
 {
+    /// <summary>
+    /// Представляет данные маршрута для передачи.
+    /// </summary>
     public class RouteDTO
     {
+        /// <summary>
+        /// Получает или задает пункт отправления маршрута.
+        /// </summary>
         public required string DeparturePoint { get; set; }
+
+        /// <summary>
+        /// Получает или задает пункт назначения маршрута.
+        /// </summary>
         public required string ArrivalPoint { get; set; }
+
+        /// <summary>
+        /// Получает или задает дату и время отправления маршрута.
+        /// </summary>
         public required DateTime DepartureDateTime { get; set; }
     }
 }
--- Domains/IServices/IAuthService.cs
+++ Domains/IServices/IAuthService.cs
@@ -3,10 +3,23 @@ using BusStationPlatform.Domains.DTO;
 
 namespace BusStationPlatform.Domains.IServices
 {
+    /// <summary>
+    /// Интерфейс для сервиса аутентификации пользователей.
+    /// </summary>
     public interface IAuthService
     {
+        /// <summary>
+        /// Регистрирует нового пользователя.
+        /// </summary>
+        /// <param name="user">Данные нового пользователя.</param>
+        /// <returns>Зарегистрированный пользователь или null, если регистрация не удалась.</returns>
         public Task<User?> RegisterAsync(User user);
 
+        /// <summary>
+        /// Аутентифицирует пользователя на основе предоставленных данных.
+        /// </summary>
+        /// <param name="loginDTO">Данные для аутентификации пользователя.</param>
+        /// <returns>Пользователь, если аутентификация успешна; иначе null.</returns>
         public Task<User?> LoginAsync(LoginDTO loginDTO);
     }
 }
